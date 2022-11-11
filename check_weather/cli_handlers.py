247ee3ab8c0fe5e65843feac0dd57d2843a592e7
Parviz Yuldashev <www.parviz98@ya.ru>
2022-11-11 19:43:23 +0500

    Added helper function

--- check_weather/cli_handlers.py
+++ check_weather/cli_handlers.py
@@ -3,7 +3,7 @@
 
 import typer
 
-from check_weather import ERRORS, api_req, config
+from check_weather import ERRORS, api_req, config, weather
 
 
 app = typer.Typer()
@@ -11,7 +11,8 @@ app = typer.Typer()
 
 @app.command()
 def init() -> None:
-    """Initialize the OpenWeather API key"""
+    """Initialize the OpenWeather API key."""
+
     api_key = typer.prompt('Please input OpenWeather API key')
 
     api_key_check_error = api_req.check_api_key(api_key)
@@ -21,7 +22,7 @@ def init() -> None:
             fg=typer.colors.RED,
         )
         raise typer.Exit(1)
-    
+
     init_error = config.init_app(api_key)
     if init_error:
         typer.secho(
@@ -30,4 +31,37 @@ def init() -> None:
         )
         raise typer.Exit(1)
 
-    typer.secho(f"Congratulations, your API key is valid! Now you can use this app :)", fg=typer.colors.GREEN)
+    typer.secho(
+        f"Congratulations, your API key is valid! Now you can use this app :)",
+        fg=typer.colors.GREEN
+    )
+
+
+def get_forecast() -> weather.Forecast:
+    """
+    Helper function
+
+    The function checks the existence of the config file and its structure.
+    Stops the program when errors occur.
+    Returns "Forecast" object for further interaction with the API.
+    """
+
+    if config.CONFIG_FILE_PATH.exists():
+        api_key = api_req.get_api_key(config.CONFIG_FILE_PATH)
+    else:
+        typer.secho(
+            'Config file not found. Please, run "check_weather init"',
+            fg=typer.colors.RED,
+        )
+        raise typer.Exit(1)
+
+    if isinstance(api_key, str):
+        return weather.Forecast(api_key)
+    else:
+        typer.secho(
+            f'FAILED with {ERRORS[api_key]}. Please, run "check_weather init"',
+            fg=typer.colors.RED,
+        )
+        raise typer.Exit(1)
+
+
